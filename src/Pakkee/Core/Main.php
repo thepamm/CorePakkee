4091dd13552b4b23cdb09b59ff2018fa3c1d14c2
MikeGamer YT <33052432+MikeGamerYT@users.noreply.github.com>
2018-12-04 17:25:46 -0600

    Update Main.php

--- src/Pakkee/Core/Main.php
+++ src/Pakkee/Core/Main.php
@@ -6,9 +6,17 @@ namespace Pakkee\Core;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-
+use pocketmine\Player;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\utils\Config;
+ 
 class Main extends PluginBase implements Listener{
  
 	public function onEnable() : void{
@@ -20,7 +28,14 @@ class Main extends PluginBase implements Listener{
 		$this->getLogger()->info("§cCorePakkee Detenido");
 	}
 	
-	
+	public function giveGadgets($player){
+    	$player->getInventory()->clearAll();
+  	$player->getInventory()->setItem(0, Item::get(369, 0, 1)->setCustomName("§7Hide Players"));
+      $player->getInventory()->setItem(8, Item::get(403, 0, 1)->setCustomName("§aPlayers On/Off"));
+      $player->getInventory()->setItem(2, Item::get(399, 0, 1)->setCustomName("§bGames"));
+      $player->getInventory()->setItem(6, Item::get(377, 0, 1)->setCustomName("§cInfo"));
+      $player->getInventory()->setItem(4, Item::get(345, 0, 1)->setCustomName("§eUtils"));
+    }
 }
 
 ?>
