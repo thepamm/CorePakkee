3de97f048e4089ad94989bbfcad447fde0960cfc
thepamm <32946286+thepamm@users.noreply.github.com>
2018-12-04 17:34:01 -0500

    Update Main.php

--- src/Pakkee/Core/Main.php
+++ src/Pakkee/Core/Main.php
@@ -1,1 +1,22 @@
+<?php
 
+declare(strict_types=1);
+
+namespace Pakkee\Core;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase{
+
+	public function onEnable() : void{
+		$this->getLogger()->info("§aCorePakkee Cargado");
+	}
+
+	public function onDisable() : void{
+		$this->getLogger()->info("§cCorePakkee Detenido");
+	}
+}
+
+?>
