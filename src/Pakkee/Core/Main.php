d615cc38b67d043cce1569c69a1bd1eb234247e1
thepamm <32946286+thepamm@users.noreply.github.com>
2018-12-04 18:54:56 -0500

    Update Main.php

--- src/Pakkee/Core/Main.php
+++ src/Pakkee/Core/Main.php
@@ -5,7 +5,22 @@ declare(strict_types=1);
 namespace Pakkee\Core;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+use pocketmine\command\CommandSender;
+use pocketmine\event\player\{PlayerInteractEvent, PlayerMoveEvent, PlayerRespawnEvent, PlayerJoinEvent, PlayerDeathEvent, PlayerQuitEvent , PlayerItemHeldEvent};
+use pocketmine\level\Position;
+use pocketmine\item\Item;
+use pocketmine\item\ItemIds;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\enchantment\ProtectionEnchantment;
+use pocketmine\command\Command;
 use pocketmine\event\Listener;
+use jojoe77777\FormAPI;
+use pocketmine\event\entity\{EntityDamageEvent, EntityDamageByEntityEvent};
+
 class Main extends PluginBase implements Listener{
  
 	public function onEnable() : void{
@@ -25,10 +40,10 @@ class Main extends PluginBase implements Listener{
 	public function giveGadgets($player){
     	$player->getInventory()->clearAll();
   	$player->getInventory()->setItem(0, Item::get(369, 0, 1)->setCustomName("§7Hide Players"));
-      $player->getInventory()->setItem(8, Item::get(403, 0, 1)->setCustomName("§aPlayers On/Off"));
-      $player->getInventory()->setItem(2, Item::get(399, 0, 1)->setCustomName("§bGames"));
-      $player->getInventory()->setItem(6, Item::get(377, 0, 1)->setCustomName("§cInfo"));
-      $player->getInventory()->setItem(4, Item::get(345, 0, 1)->setCustomName("§eUtils"));
+      	$player->getInventory()->setItem(8, Item::get(403, 0, 1)->setCustomName("§aPlayers On/Off"));
+      	$player->getInventory()->setItem(2, Item::get(399, 0, 1)->setCustomName("§bGames"));
+      	$player->getInventory()->setItem(6, Item::get(377, 0, 1)->setCustomName("§cInfo"));
+      	$player->getInventory()->setItem(4, Item::get(345, 0, 1)->setCustomName("§eUtils"));
     }
 }
 
